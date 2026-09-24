f7eb366e38267b4845ff5cd8adcf40eeb3bad3f7
Michael Hillcox <michaelhillcox@gmail.com>
2026-09-24 22:39:46 +0100

    feat: helper method on RegistryHolder

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## [26.3.0.2]
+
+### Added
+* `TagKey<T> createKey(ResourceKey<Registry<T>> registryKey)` helper method to the RegistryHolder interface
+
 ## [26.3.0.1]
 
 ### Changed
--- common/src/main/java/dev/nanite/library/core/registry/RegistryHolder.java
+++ common/src/main/java/dev/nanite/library/core/registry/RegistryHolder.java
@@ -1,6 +1,9 @@
 package dev.nanite.library.core.registry;
 
+import net.minecraft.core.Registry;
 import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.TagKey;
 
 import java.util.function.Supplier;
 
@@ -9,4 +12,8 @@ public interface RegistryHolder<R, T extends R> extends Supplier<T> {
     T get();
 
     Identifier identifier();
+
+    default TagKey<T> createKey(ResourceKey<Registry<T>> registryKey) {
+        return TagKey.create(registryKey, identifier());
+    }
 }
