93c319ebc4a8612d6a1e0d3d8518ebc0d3dbf4a8
Michael Hillcox <michaelhillcox@gmail.com>
2026-09-24 22:46:37 +0100

    fix: more helpers

--- common/src/main/java/dev/nanite/library/core/registry/RegistryHolder.java
+++ common/src/main/java/dev/nanite/library/core/registry/RegistryHolder.java
@@ -13,7 +13,19 @@ public interface RegistryHolder<R, T extends R> extends Supplier<T> {
 
     Identifier identifier();
 
-    default TagKey<R> createKey(Registry<R> registry) {
+    default TagKey<R> createTagKey(Registry<R> registry) {
         return TagKey.create(registry.key(), identifier());
+    }
+
+    default TagKey<R> createTagKey(ResourceKey<? extends Registry<R>> registryKey) {
+        return TagKey.create(registryKey, identifier());
+    }
+
+    default ResourceKey<T> createResourceKey(ResourceKey<? extends Registry<T>> registryKey) {
+        return ResourceKey.create(registryKey, identifier());
+    }
+
+    default ResourceKey<T> createResourceKey(Registry<T> registry) {
+        return ResourceKey.create(registry.key(), identifier());
     }
 }
